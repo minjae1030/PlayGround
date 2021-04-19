3316417b339e17e9decb7f116bb9086521ccc932
minjae1030 <minjae103030@naver.com>
2021-04-19 11:12:03 +0900

    final

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # PlayGround
   * Main
     - Input data -> save as CSV file
+    - ConstValueClass.java의 PATH = "본인 컴퓨터 저장경로\\PlayGround.csv"로 변경하세요.
+    - ConstValueClass.java의 PATH1 = "본인 컴퓨터 저장경로\\Report.csv"로 변경하세요.
 
   * CSV function
     - Read CSV file -> Parse data (day , kind of ticket , kind of DC)
+    - ConstValueClass.java의 PATH = "본인 컴퓨터 저장경로\\PlayGround.csv"로 변경하세요.
+    - ConstValueClass.java의 PATH1 = "본인 컴퓨터 저장경로\\Report.csv"로 변경하세요.
+    - ConstValueClass.java의 PATH2 = "본인 컴퓨터 저장경로\\PerdaySellamount.csv"로 변경하세요.
+    - ConstValueClass.java의 PATH3 = "본인 컴퓨터 저장경로\\PerTicketSales.csv"로 변경하세요.
---
+++ advance/src/playpark/Main.java
@@ -0,0 +1,42 @@
+package playpark;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		InputClass inputClass = new InputClass();
+		RunClass runClass = new RunClass();
+		OutputClass outClass = new OutputClass();
+	
+		int ticketSelect = 0, orderCount = 0, dcSelect = 0, priceResult = 0, choice = 0, total_price = 0;
+		int choice_final = 0, age = 0;
+		String customerIDNumber = "";
+		
+		do {
+			while (true) {
+				ticketSelect = inputClass.inputTicketSelect();
+				customerIDNumber = inputClass.inputIDnumber();
+				orderCount = inputClass.inputOrderCount();
+				dcSelect = inputClass.inputDcSelect();
+				age = runClass.calAge(customerIDNumber);
+				priceResult = runClass.calProcess(customerIDNumber, ticketSelect, dcSelect, orderCount, priceResult,
+						age);
+				total_price += priceResult;
+				runClass.saveOrder(ticketSelect, age, orderCount, priceResult, dcSelect, ConstValueClass.cusInfoArr);
+				outClass.printPrice(priceResult);
+				choice = inputClass.checkContinue();
+				if (choice == ConstValueClass.CHOICE_EXIT) {
+					break;
+				}
+			}
+			outClass.orderPrint(total_price, ConstValueClass.cusInfoArr);
+			outClass.saveFILE(ConstValueClass.cusInfoArr);
+			choice_final = inputClass.checkFINAL();
+		} while (choice_final == 1);
+	
+	}
+	
+	
+}
