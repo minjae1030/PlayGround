e50a20294a2904c453105dd910c356298b04ad29
minjae1030 <minjae103030@naver.com>
2021-04-19 15:21:43 +0900

    change path1

--- readCSV/src/readCSV/ConstValueClass.java
+++ readCSV/src/readCSV/ConstValueClass.java
@@ -2,7 +2,7 @@ package readCSV;
 
 public class ConstValueClass {
 	public static String PATH = "PlayGround.csv";
-	public static String PATH1 = "C:\\Users\\kmj\\eclipse-workspace\\advance\\Report.csv";
+	public static String PATH1 = "Report.csv";
 	public static String PATH2 = "PerDaySellamount.csv";
 	public static String PATH3 = "PerTicketSales.csv";
 	public static String INFANT = "유아", CHILDREN = "어린이", JUVENILE = "청소년",
